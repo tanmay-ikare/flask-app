aaa28452740da0dda10712b45497f9dfbe99cf41
tanmay <tanmay.ikare@helpshift.com>
2025-04-08 22:12:07 +0530

    adding host

--- app.py
+++ app.py
@@ -161,4 +161,4 @@ def download_file(filename):
     return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
